1d717e9404d521ad2f3ed9a66e7303cb5899a180
Kevin Li <kevinlishirt@berkeley.edu>
2021-11-18 22:59:19 -0500

    Adding all the files

---
+++ mapper.py
@@ -0,0 +1,6 @@
+import sys
+for line in sys.stdin:
+    line = line.strip()
+    temperature = int(line[87:92])
+    if ((temperature != 9999)):
+        print('%s\t%d' % (line[15:23], int(line[87:92])))
